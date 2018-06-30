ae093dd07f773dc57800c8580ef949e5b8529047
Thomas Riley <rileyt@ohsu.edu>
2018-06-29 19:44:14 -0700

    Update deps, remove minify

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,6 @@ module.exports = {
 			include: /src/,
 			exclude: /node_modules/,
 			loader: 'babel-loader',
-		}, ],
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production'),
-			},
-		}),
-		new webpack.optimize.UglifyJsPlugin(),
-	],
+		}]
+	}
 };
